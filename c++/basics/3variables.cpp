721389c2da59d4f5b60135cac180385ec1de5310
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-07-08 08:19:27 +0530

    Add files via upload

---
+++ c++/basics/3variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    x = 5521;
+    cout<<x<<endl; // we specified that x is an int so x will be printed as an integer
+    int y, z=22;
+    y = 5521;
+    cout<<y<<endl; // similar to variable x
+    int sum=x+y;
+    cout<<sum; // first sums the x and y 
+}
