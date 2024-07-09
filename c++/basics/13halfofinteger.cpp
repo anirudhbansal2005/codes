1e88e928eb97e972cd8f72b30fae37d99a18742a
CloudyOrk <120650272+CloudyOrk@users.noreply.github.com>
2024-07-09 22:39:51 +0530

    Add files via upload

---
+++ c++/basics/13halfofinteger.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int x;
+    cout<<"Enter digit: ";
+    cin>>x;
+    float half = ((float)x)/2; // typecasting
+    cout<<"Half of "<<x<<" is "<<half;
+    
+}
---
+++ c++/basics/15intToChar.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout<<"Enter digit to covert: ";
+    cin>>x;
+    char ch = (char)x;
+    cout<<"Character at ASCII value "<<x<<" is "<<ch;
+
+    
+
+
+ }
