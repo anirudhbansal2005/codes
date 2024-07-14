72a217ad921091b1073ff7d414e071f91d6d66ed
CloudyOrk <anirudhbansal006@gmail.com>
2024-07-14 23:27:53 +0530

    real

--- functions/04_pascalTriangle.cpp
+++ functions/04_pascalTriangle.cpp
@@ -1,5 +1,26 @@
 #include<iostream>
 using namespace std;
+int factorial(int x){
+    int pc = 1;
+    for( int i=1; i<=x; i++){
+        pc *= i;
+    }
+
+    return pc;
+}
+
+int pnc(int y, int z){
+    return aa/()
+}
+
 int main(){
-    // test
+    int col=1, row=1, diff, aa=1, bb=1;
+    for (int i=1; i<=5; i++){
+        aa = factorial(col);
+        bb = factorial(row);
+
+        cout<<aa<<endl;
+        cout<<aa<<endl;
+        col++;
+    }
 }
