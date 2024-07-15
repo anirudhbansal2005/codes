a3c5665d035a5c8a60113fdea3e720dc963c9112
CloudyOrk <anirudhbansal006@gmail.com>
2024-07-15 16:21:20 +0530

    fake

--- functions/04_pascalTriangle.cpp
+++ functions/04_pascalTriangle.cpp
@@ -5,22 +5,26 @@ int factorial(int x){
     for( int i=1; i<=x; i++){
         pc *= i;
     }
-
     return pc;
 }
 
-int pnc(int y, int z){
-    return aa/()
+int pnc(int n, int r, int p){
+    return n/(r*p);
 }
 
 int main(){
-    int col=1, row=1, diff, aa=1, bb=1;
-    for (int i=1; i<=5; i++){
+    int col=1, row=3, diff, aa=1, bb=1, cc=1, triangle=1;
+    for (int k=1; k<=5; i++){
         aa = factorial(col);
         bb = factorial(row);
-
+        cc = factorial(row-col);
+        triangle = pnc( bb,aa, cc);
         cout<<aa<<endl;
-        cout<<aa<<endl;
+        cout<<bb<<endl;
+        cout<<cc<<endl;
+        cout<<triangle<<endl;
         col++;
+        row++;
+        for ( int i = 1;  )
     }
 }
